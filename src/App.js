8303bed4542072250b7d665adb8faa282c948eb0
Rohan Kokane <rohankokane19@gmail.com>
2022-01-13 15:01:50 +0530

    tooling config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      Bookworm
-    </div>
-  );
+  return <div className="App">Bookworm</div>
 }
 
-export default App;
+export default App
