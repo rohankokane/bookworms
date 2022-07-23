12b3a9aa98b6d2b0e3df904023a5760e669ecf84
Rohan Kokane <rohankokane19@gmail.com>
2022-07-23 17:33:08 +0530

    code split, remove external css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-// import { lazy, Suspense } from 'react'
+import { lazy, Suspense } from 'react'
 import { useAuth } from 'hooks/auth-hook'
-import AuthenticatedApp from 'AuthenticatedApp'
-import UnauthenticatedApp from 'UnauthenticatedApp'
-// const AuthenticatedApp = lazy(() =>
-//   import(/* webpackPrefetch: true */ './AuthenticatedApp')
-// )
-// const UnauthenticatedApp = lazy(() => import('./UnauthenticatedApp'))
+import StartupScreen from 'screens/StartupScreen'
+const AuthenticatedApp = lazy(() =>
+  import(/* webpackPrefetch: true */ './AuthenticatedApp')
+)
+const UnauthenticatedApp = lazy(() => import('./UnauthenticatedApp'))
 
 function App() {
   const { token } = useAuth()
   return (
-    // <Suspense fallback={'Loading...'}>
-    token ? <AuthenticatedApp /> : <UnauthenticatedApp />
-    // </Suspense>
+    <Suspense fallback={<StartupScreen />}>
+      {token ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+    </Suspense>
   )
 }
 
--- src/context/index.js
+++ src/context/index.js
@@ -4,11 +4,30 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from 'store'
 import AuthProvider from './AuthProvider'
+import { Global, css } from '@emotion/react'
 
 function AppProviders({ children }) {
   return (
     <>
       <ChakraProvider theme={theme}>
+        <Global
+          styles={css`
+            body {
+              margin: 0;
+              -webkit-font-smoothing: antialiased;
+              -moz-osx-font-smoothing: grayscale;
+              overflow-y: scroll;
+            }
+
+            input,
+            textarea,
+            button,
+            select,
+            a {
+              -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+            }
+          `}
+        />
         <CSSReset />
         <Provider store={store}>
           <Router>
